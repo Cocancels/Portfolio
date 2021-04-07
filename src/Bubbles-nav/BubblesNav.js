614832ed13f91407122160653bd1ec3a4358f2de
CocsAncel <corentin.ancel@edu.devinci.fr>
2021-04-07 22:16:20 +0200

    Adding responsives for small devices

--- src/Bubbles-nav/BubblesNav.js
+++ src/Bubbles-nav/BubblesNav.js
@@ -1,15 +1,44 @@
 import "./BubblesNav.css";
 import React from "react";
+import {
+  Link,
+} from "react-scroll";
 
 
 export function BubblesNav() {
   return (
     <div className="bubbles">
-      <div className="bubble" id="bubble-header"></div>
+       <Link
+        activeClass="active"
+        className="button-link"
+        to="header"
+        spy={true}
+        smooth={true}
+        duration={1000}
+      >
+        <div className="bubble" id="bubble-header"></div>
+      </Link>
 
-      <div className="bubble" id="bubble-presentation"></div>
-
-      <div className="bubble" id="bubble-projets"></div>
+      <Link
+        activeClass="active"
+        className="button-link"
+        to="presentation"
+        spy={true}
+        smooth={true}
+        duration={1000}
+      >
+        <div className="bubble" id="bubble-presentation"></div>
+      </Link>
+      <Link
+        activeClass="active"
+        className="button-link"
+        to="projets"
+        spy={true}
+        smooth={true}
+        duration={1000}
+      >
+        <div className="bubble" id="bubble-projets"></div>
+      </Link>
     </div>
   );
 }
